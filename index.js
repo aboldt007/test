d146ebbabada534c5f64475018b8cead5a6eaff2
aboldt007 <64077465+aboldt007@users.noreply.github.com>
2020-05-01 11:52:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var myElement = document.getElementById('myElement');
 
 // create a simple instance
 // by default, it only adds horizontal recognizers
-var mc = new Hammer(myElement);
+var mm = new Hammer(myElement);
 
 // listen to events...
-mc.on("panleft panright tap press", function(ev) {
+mm.on("panleft panright tap press", function(ev) {
     myElement.textContent = ev.type +" gesture detected.";
 });
